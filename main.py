c291e8aca110bd44e999d09806ecc21336ad282c
yathendra5 <yathendra.Ambati@kasmodigital.com>
2026-03-30 11:55:16 +0530

    Remove commented sections from main.py

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 # main.py
-from extract import customer_dim        # function from your extract.py
-from transform import transform_simple  # function from your transform.py
-from load import load                   # function from your load.py
+from extract import customer_dim       
+from transform import transform_simple  
+from load import load                  
 import pandas as pd
 
 if __name__ == "__main__":
-    # -------------------------
-    # 1️⃣ Extract
-    # -------------------------
+  
     dim_customer_df, updates_df = customer_dim()
     print(f"[EXTRACT] Loaded dim_customer ({len(dim_customer_df)} rows) and updates ({len(updates_df)} rows)")
 
-    # -------------------------
-    # 2️⃣ Transform
-    # -------------------------
     expire_df, insert_df = transform_simple(dim_customer_df, updates_df)
     print(f"[TRANSFORM] Expire rows: {len(expire_df)}, Insert rows: {len(insert_df)}")
 
-    # Create a simple change log DataFrame
+   
     change_list = []
 
     for _, row in insert_df.iterrows():
@@ -30,9 +25,6 @@ if __name__ == "__main__":
 
     change_df = pd.DataFrame(change_list)
     print(f"[TRANSFORM] Change log rows: {len(change_df)}")
-
-    # -------------------------
-    # 3️⃣ Load
-    # -------------------------
+    
     load(expire_df, insert_df, change_df)
     print("[MAIN] ETL finished successfully!")
